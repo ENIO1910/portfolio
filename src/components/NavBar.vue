e0dc1ac3390363b86c4a748660cec42c7d6af746
ENIO1910 <patrykstaniewski1910@gmail.com>
2023-05-09 11:59:00 +0200

    x

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-app-bar app color="black" dark flat class="px-12">
+        <v-btn class="d-flex ">
+            <div class="d-flex align-baseline">
+                <v-icon color="warning" left class="mr-2">fa-solid fa-robot</v-icon>
+                <p class="ml-2" style="font-size:14px;">Patryk Staniewski</p>
+            </div>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn text @click="scroll('home')" class="text-orange">Home</v-btn>
+        <v-btn text @click="scroll('About')" class="text-white">About</v-btn>
+        <v-btn text @click="scroll('portfolio')" class="text-white">portfolio</v-btn>
+        <v-btn text @click="scroll('services')" class="text-white">services</v-btn>
+        <v-btn text @click="scroll('page')" class="text-white">page</v-btn>
+        <v-btn text @click="scroll('contact')" class="text-white">contact</v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+  export default {
+      methods: {
+          scroll(refName) {
+              const element = document.getElementById(refName);
+              element.scrollIntoView({behavior: "smooth"});
+
+          }
+      }
+  }
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,116 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="home">
+      <NavBar />
+      <v-container fluid>
+        <div class="head">
+          <v-row>
+              <v-col cols="5">
+                  <div style="position:relative" class="mt-16">
+                      <h1 class="text-white">Witaj,</h1>
+                      <h1 class="text-grey">Nazywam się</h1>
+                      <h1 class="text-white">Patryk Staniewski</h1>
+                      <span class="text-grey">Web Designer & Developer</span><br />
+                      <v-btn title dark class="text-orange mt-8" variant="outlined">Contact me</v-btn>
+                  </div>
+              </v-col>
+              <v-col cols="2">
+                  <div style="position:absolute; z-index: 999; bottom:0; margin-left:auto; margin-right: auto; left:0; right: 0; text-align: center;">
+                      <v-icon>fas fa-angle-double-down</v-icon>
+                  </div>
+              </v-col>
+              <v-col cols="5">
+                  <div style="position:relative; z-index: 999;" class="mt-16 pr-16">
+                      <v-img src="img/me.png" contain max-height="350" ></v-img>
+                  </div>
+
+              </v-col>
+          </v-row>
+        </div>
+          <v-col cols="12" class="mt-16" id="about">
+            <div>
+                <v-row>
+                    <v-col cols="12" sm="6">
+                        <div class="egg">
+                            <v-img src="img/i2.png" max-height="300"></v-img>
+                        </div>
+                    </v-col>
+                    <v-col cols="12" sm="6">
+                        <h5 class="mt-16">About Me</h5>
+                        <div style="width: 120px">
+                            <v-slider v-model="slider2" color="orange"></v-slider>
+                        </div>
+                    </v-col>
+                </v-row>
+            </div>
+          </v-col>
+      </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent } from 'vue';
 
-export default {
+// Components
+import NavBar from '../components/NavBar.vue';
+
+export default defineComponent({
   name: 'HomeView',
+    setup(){
+      return {
+       slider2: 50,
+          items: [
+              {
+                  img: "img/i2.png"
+              },
+          ],
+      };
+    },
+
   components: {
-    HelloWorld
-  }
-}
+    NavBar,
+  },
+});
 </script>
+<style scoped>
+.v-container{
+    padding:16px 0;
+}
+.head {
+    position: relative;
+    text-align: center;
+    padding:12px;
+    margin-bottom: 6px;
+    height:400px;
+    width:100%;
+    color: white;
+}
+.head:before{
+    content: " ";
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 50%;
+    background: black;
+    transform: skew(0deg, 6deg);
+}
+.head:after{
+    content: " ";
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 100%;
+    width: 50%;
+    background: black;
+    transform: skew(0deg, -6deg);
+}
+.egg{
+    display: block;
+    margin-left: 100px;
+    margin-top: 50px;
+    width:356px;
+    height: 300px;
+    background-color: #ffa35b;
+    border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
+}
+</style>
